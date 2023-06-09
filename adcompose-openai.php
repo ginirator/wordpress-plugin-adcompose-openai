d591e86219dea5a35915c3f69e18dc337ccdeff4
Valeriu Dodon <valeriu.dodon@gmail.com>
2023-06-09 22:50:00 +0200

    Added detailed comments to PHP, jQuery, and CSS code for improved readability and maintenance.

--- adcompose-openai.php
+++ adcompose-openai.php
@@ -9,41 +9,50 @@ Author URI: https://ginirator.com/online-tools/adcompose-fast-promotional-copy-g
 License: GPL2
 */
 
+// Load scripts and styles for the plugin
 function adcompose_openai_scripts() {
-	// Use plugins_url() function to include your style file from your plugin.
-    wp_enqueue_style('adcompose_styles', plugins_url('assets/css/styles.css', __FILE__));
+	// Enqueue CSS file for the plugin
+	wp_enqueue_style('adcompose_styles', plugins_url('assets/css/styles.css', __FILE__));
 
-    wp_register_script('adcompose-openai-script', plugins_url('assets/js/scripts.js', __FILE__), array('jquery'), '1.0', true);
+	// Register the JavaScript file with WordPress's script handler
+	wp_register_script('adcompose-openai-script', plugins_url('assets/js/scripts.js', __FILE__), array('jquery'), '1.0', true);
 
-    wp_localize_script('adcompose-openai-script', 'adcompose_openai_object', array(
+	// Localize the script with additional data
+	wp_localize_script('adcompose-openai-script', 'adcompose_openai_object', array(
 		'plugin_url' => plugins_url('', __FILE__),
-        'ajax_url' => admin_url('admin-ajax.php')
-    ));
+		'ajax_url' => admin_url('admin-ajax.php')
+	));
 
-    wp_enqueue_script('adcompose-openai-script');
+	// Enqueue the script to the front-end
+	wp_enqueue_script('adcompose-openai-script');
 }
 add_action('wp_enqueue_scripts', 'adcompose_openai_scripts');
 
+// Define the shortcode function for 'adcompose'
 function adcompose_openai_form($atts = []) {
-    // Normalize attribute keys, lowercase
-    $atts = array_change_key_case((array)$atts, CASE_LOWER);
+	// Normalize attribute keys, convert to lowercase
+	$atts = array_change_key_case((array)$atts, CASE_LOWER);
 
-    // Override default attributes with user attributes
-    $adcompose_atts = shortcode_atts([
-        'apikey' => '',
-        'model' => 'text-davinci-003',
-        'maxtokens' => 100,
-    ], $atts);
+	// Override default attributes with user attributes
+	$adcompose_atts = shortcode_atts([
+		'apikey' => '',
+		'model' => 'text-davinci-003',
+		'maxtokens' => 100,
+	], $atts);
 
+	// Error handling for missing API key
 	if ( empty($adcompose_atts['apikey']) ) {
 		error_log('Error: OpenAI API key not provided in the [adcompose] shortcode parameters.');
 		echo '<p style="color: red;">Error: Please provide a valid OpenAI API key as a parameter in the shortcode.</p>';
 	} else {
 		// Store the attributes as an option
 		update_option('adcompose_openai_atts', $adcompose_atts);
+
+		// Begin output buffering
 		ob_start();
 		?>
 
+		<!-- Begin form HTML -->
 		<div id="adcompose-wrapper">
 			<h3>Welcome to AdCompose</h3>
 			<p>Get promotional copy for your products fast!</p>
@@ -65,45 +74,52 @@ function adcompose_openai_form($atts = []) {
 		</div>
 
 		<div id="ad-output"></div>
+		<!-- End form HTML -->
 
 		<?php
-		return ob_get_clean();
+		return ob_get_clean(); // End output buffering and return contents
 	}
 }
-add_shortcode('adcompose', 'adcompose_openai_form');
+add_shortcode('adcompose', 'adcompose_openai_form'); // Register the shortcode
 
+// Function to handle AJAX requests
 function adcompose_openai_handle_request() {
-    // Retrieve the global parameters
-    $adcompose_openai_global_atts = get_option('adcompose_openai_atts', array());
-    $apiKey = $adcompose_openai_global_atts['apikey'];
-    $apiModel = $adcompose_openai_global_atts['model'];
-    $apiMaxTokens = intval($adcompose_openai_global_atts['maxtokens']);
+	// Retrieve the stored attributes
+	$adcompose_openai_global_atts = get_option('adcompose_openai_atts', array());
+	$apiKey = $adcompose_openai_global_atts['apikey'];
+	$apiModel = $adcompose_openai_global_atts['model'];
+	$apiMaxTokens = intval($adcompose_openai_global_atts['maxtokens']);
 
+	// If it's a POST request
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+		// Retrieve form data
 		$productName = $_POST['productName'];
 		$productDesc = $_POST['productDesc'];
 		$productTarget = $_POST['productTarget'];
 
+		// Check for empty fields
 		if (empty($productName) || empty($productDesc) || empty($productTarget)) {
 			echo "Please fill all the fields";
 			exit;
 		}
 
+		// Define the prompt for OpenAI
 		$prompt = "Create max 50 words of an advertising copy for a product.
 		Use Product Name \"{$productName}\",
 		Description \"{$productDesc}\",
 		Target Market \"{$productTarget}\"";
 
+		// Prepare the data for the API
 		$data = [
 			'prompt' => $prompt,
 			'max_tokens' => $apiMaxTokens,
 		];
 
+		// Initialize cURL
 		$ch = curl_init();
 
+		// Set cURL options
 		curl_setopt($ch, CURLOPT_URL, 'https://api.openai.com/v1/engines/' . $apiModel . '/completions');
-
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -112,16 +128,20 @@ function adcompose_openai_handle_request() {
 			'Authorization: Bearer ' . $apiKey
 		]);
 
+		// Execute cURL request and get the response
 		$response = curl_exec($ch);
 
+		// Check for errors
 		if (curl_errno($ch)) {
 			echo 'Error:' . curl_error($ch);
 		}
 
-		curl_close($ch);
+		curl_close($ch); // Close the cURL session
 
+		// Decode the response
 		$responseData = json_decode($response, true);
 
+		// Check if there is a text response
 		if (isset($responseData['choices'][0]['text'])) {
 			echo trim($responseData['choices'][0]['text']);
 		} else {
@@ -130,7 +150,6 @@ function adcompose_openai_handle_request() {
 
 		exit;
 	}
-
 }
-add_action('wp_ajax_nopriv_adcompose_openai_handle_request', 'adcompose_openai_handle_request');
-add_action('wp_ajax_adcompose_openai_handle_request', 'adcompose_openai_handle_request');
+add_action('wp_ajax_nopriv_adcompose_openai_handle_request', 'adcompose_openai_handle_request'); // Handle AJAX request for non-authenticated users
+add_action('wp_ajax_adcompose_openai_handle_request', 'adcompose_openai_handle_request'); // Handle AJAX request for authenticated users
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,35 +1,64 @@
+// On document ready
 jQuery(document).ready(function ($) {
-	console.log( adcompose_openai_object.plugin_url + '/assets/images/loading.gif' );
-	$('#ad-input').submit(function(event) {
+
+	// Log the URL of the loading.gif in the console
+	console.log(adcompose_openai_object.plugin_url + '/assets/images/loading.gif');
+
+	// Attach an event handler to the 'submit' event of the form with id 'ad-input'
+	$('#ad-input').submit(function (event) {
+
+		// Prevent the form from submitting the default way
 		event.preventDefault();
 
+		// Get the values from the form inputs
 		const productName = $('#productName').val();
 		const productDesc = $('#productDesc').val();
 		const productTarget = $('#productTarget').val();
 
+		// Check if all fields have been filled
 		if (!productName || !productDesc || !productTarget) {
+
+			// Alert the user if a field is missing
 			alert("Please fill all the fields");
 			return;
 		}
 
+		// Replace the form content with a loading gif while processing
 		$('#ad-input').html('<img src="' + adcompose_openai_object.plugin_url + '/assets/images/loading.gif" alt="Loading...">');
 
-
+		// Make an AJAX request to the server
 		$.ajax({
+			// Define the URL for the AJAX request
 			url: adcompose_openai_object.ajax_url,
+
+			// Set the HTTP method to POST
 			method: 'POST',
+
+			// Define the data to be sent in the AJAX request
 			data: {
 				action: 'adcompose_openai_handle_request',
 				productName: productName,
 				productDesc: productDesc,
 				productTarget: productTarget,
 			},
-			success: function(data) {
+
+			// Define a function to handle a successful response
+			success: function (data) {
+
+				// Hide the input form
 				$('#ad-input').attr('style', 'display: none;');
+
+				// Show the output area
 				$('#ad-output').attr('style', 'display: block;');
+
+				// Fill the output area with the response data
 				$('#ad-output-copy').html(data);
 			},
-			error: function(jqXHR, textStatus, errorThrown) {
+
+			// Define a function to handle errors
+			error: function (jqXHR, textStatus, errorThrown) {
+
+				// Output the error in the ad-output div
 				$('#ad-output').html('There was an error: ' + errorThrown);
 			}
 		});
